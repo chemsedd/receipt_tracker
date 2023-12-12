cfb3e42756c88f0d65b793a1e00cddfcb5d675a7
Chems Eddine Senoussi <chemseddsen@gmail.com>
2023-12-12 21:19:25 +0100

    Create Receipt model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Receipt(models.Model):
+    """
+    Receipt information
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    store_name = models.CharField(max_length=255)
+    date_of_purchase = models.DateField()
+    item_list = models.TextField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
